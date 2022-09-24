9611874ef232fc56b7627950c18770682a096546
Tasfiq97 <tasfiqulazad200@gmail.com>
2022-09-25 00:30:23 +0600

    vide gallery

---
+++ fontendfiles/src/components/Video Gallery/VideoGalllery.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const VideoGalllery = () => {
+    return (
+        <div className='container w-2/3 mx-auto mt-10 '>
+           <div className=''>
+            <h1 className='text-3xl text-red-600 font-semibold text-left mb-5'>Videos</h1>
+           </div>
+           <hr />
+           <div className='mt-10 flex flex-row justify-center mb-10'>
+           <iframe width="320" height="330" src="https://www.youtube.com/embed/hzQOCsaQvK0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Vinita Srivastava, National Senior consultant & co-ordinator Blood cell NHM Delhi</p>
+           </div>
+           <hr />
+           <div className='mt-10 flex flex-row justify-center mb-10' >
+           <iframe width="320" height="330" src="https://www.youtube.com/embed/6IUjcLZnbss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Mr Manish Choudhary, State coordinator officer blood cell NHM Rajasthan</p>
+           </div>
+           <hr />
+           <div className='mt-10 flex flex-row justify-center mb-10'>
+           <iframe className='mr-12' width="320" height="330" src="https://www.youtube.com/embed/crVglHe4tHA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Dr Mahendra Kendre, ADHS blood cell DHS Mumbai</p>
+           </div>
+           <hr />
+           <div className='mt-10 flex flex-row justify-center mb-10'>
+           <iframe width="320" height="330" src="https://www.youtube.com/embed/X97vd6BnELs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Dr. J.S. Arora, General Secretary at National Thalassemia Welfare Society & Federation of Indian Thalassemia, New Delhi</p>
+           </div>
+           <hr />
+           <div className='mt-10 flex flex-row justify-center mb-10'>
+           <iframe width="320" height="330" src="https://www.youtube.com/embed/ciJMbfT0xlg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Message By : Dr Ruby Khan, Deputy Director, Blood Cell NHM, Madhya Pradesh</p>
+           </div>
+           <hr />
+           <h1 className='text-3xl text-red-500 font-semibold'>Message From Kids</h1>
+           <div className='mt-10 flex flex-row justify-center mb-10'>
+           <iframe width="320" height="330" src="https://www.youtube.com/embed/_IRqKTgJdDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Kids Message: Let's Do it. Donate Blood for Thalassemia patient</p>
+           </div>
+           <div className='mt-10 mr-20 flex flex-row justify-center mb-10'>
+           <iframe width="320" height="330" src="https://youtube.com/embed/bTiws2YloYI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>Kids Life saving Message : Let's do it. Donate Blood</p>
+           </div>
+           <hr />
+           <h1 className='text-3xl text-red-500 font-semibold'>Message From Users</h1>
+           <div className='mt-10 mr-96 flex flex-row justify-center'>
+           <iframe width="320" height="330" src="https://youtube.com/embed/W97uuaZ9C5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>User's feedback</p>
+           </div>
+           <div className='mt-10 mr-96 flex flex-row justify-center'>
+           <iframe width="320" height="330" src="https://youtube.com/embed/wvF32eT5S88" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>User's feedback</p>
+           </div>
+           <div className='mt-10  mr-96 flex flex-row justify-center'>
+           <iframe width="320" height="330" src="https://youtube.com/embed/qKesV5s0rcw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>User's Feedback</p>
+           </div>
+           <div className='mt-10 mr-96 flex flex-row justify-center'>
+           <iframe width="320" height="330" src="https://youtube.com/embed/zVMz6xPXT-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <p className='p-10 text-2xl mt-10 text-gray-600'>User's Feedback</p>
+           </div>
+          
+          
+        </div>
+    );
+};
+
+export default VideoGalllery;
