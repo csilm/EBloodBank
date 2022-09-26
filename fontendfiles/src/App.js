98cc782ccfd5bf737c590f0f9267af5642114985
Showrav-das <showravdas8@gmail.com>
2022-09-26 14:27:20 +0600

    navbar footer added

--- fontendfiles/src/App.js
+++ fontendfiles/src/App.js
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
 import './App.css';
 import VideoGalllery from './components/Video Gallery/VideoGalllery';
-=======
->>>>>>> 11d033b44b00eb3df4b3efc05571a8d6ef8970f6
+import Faq from './components/F.A.Q/Faq';
+import Footer from './components/Footer/Footer';
+import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css';
-import Faq from './components/F.A.Q/Faq'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+      <Navigation />
+     
      <VideoGalllery></VideoGalllery>
-=======
       <Faq/>
->>>>>>> 11d033b44b00eb3df4b3efc05571a8d6ef8970f6
+      <Footer />
+     
     </div>
   );
 }
