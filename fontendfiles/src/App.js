bb20d5965e433ffa0df3f9261820337a7aefbc01
Tasfiq97 <tasfiqulazad200@gmail.com>
2022-09-29 16:56:28 +0600

    login register

--- fontendfiles/src/App.js
+++ fontendfiles/src/App.js
@@ -1,20 +1,30 @@
-<<<<<<< HEAD
+
 import './App.css';
 import VideoGalllery from './components/Video Gallery/VideoGalllery';
-=======
->>>>>>> 11d033b44b00eb3df4b3efc05571a8d6ef8970f6
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import './App.css';
 import Faq from './components/F.A.Q/Faq'
+import BloodStock from './components/Blood Stock/BloodStock';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-     <VideoGalllery></VideoGalllery>
-=======
-      <Faq/>
->>>>>>> 11d033b44b00eb3df4b3efc05571a8d6ef8970f6
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<VideoGalllery></VideoGalllery>}>
+      </Route>
+      <Route path="/faq" element={<Faq></Faq>} ></Route>
+      <Route path="/bloodstock" element={<BloodStock></BloodStock>} ></Route>
+      <Route path="/login" element={<Login></Login>} ></Route>
+      <Route path="/register" element={<Register></Register>} ></Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
